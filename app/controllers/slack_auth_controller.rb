575627c5e1f9f23e916bd0aeae378ee421f25aa4
Amit Kumar <amit@mavenhive.in>
2017-02-22 12:49:03 +0530

    Slack button requests route throught the python slack app

--- app/controllers/slack_auth_controller.rb
+++ app/controllers/slack_auth_controller.rb
@@ -2,26 +2,16 @@ require 'http'
 
 class SlackAuthController < ApplicationController
   def initiate_auth
-    slack_url = 'https://slack.com/oauth/authorize'
-    client_id = Rails.application.secrets.client_id
-    puts "CLIENT_ID IS : #{client_id}"
-    params = {'client_id': client_id,
-              'scope': 'incoming-webhook,commands,bot',
-              'redirect_uri': 'https://6e5224bb.ngrok.io/authresponse'
-    }
-    auth_url = generate_url(slack_url, params)
+    slack_app_url = Rails.application.secrets.slack_app
+    params = {'redirect_to': 'https://6e5224bb.ngrok.io/authresponse'}
+    auth_url = generate_url(slack_app_url, params)
     redirect_to auth_url
   end
 
   def auth_response
-    temp_code = params[:code]
-    token_url = 'https://slack.com/api/oauth.access'
-    params = {'client_id': Rails.application.secrets.client_id,
-              'client_secret': Rails.application.secrets.client_secret,
-              'code': temp_code
-    }
-    auth_url = generate_url(token_url, params)
-    @resp = HTTP.get(auth_url).parse
+    @resp = params
+    puts "THE RESPONSE IS : #{@resp}"
+    puts @resp[:ok]
     render 'slack_auth/auth_response'
   end
 end
